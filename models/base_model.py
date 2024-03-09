bf444a68865e088032491f25ceda94998e0faab8
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-03-09 14:37:34 +0100

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from datetime import datetime
 import uuid
+from datetime import datetime
+
+from models.engine import storage  # noqa: F401
 
 """BaseModel class"""
 
@@ -8,15 +10,18 @@ import uuid
 class BaseModel:
     """BaseModel class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """init method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-    def __str__(self):
-        """str method"""
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    value = datetime.strptime(value, time_format)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Changes the updated_at attribute to the current datetime
@@ -30,3 +35,9 @@ class BaseModel:
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
+
+    def __str__(self):
+        """str method"""
+        return_str = "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
+        return return_str
