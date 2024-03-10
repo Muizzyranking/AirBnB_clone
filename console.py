7feab8c9d36a23ebc739831b0b4008768eb757fc
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-03-10 19:56:43 +0100

    console 001

--- console.py
+++ console.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python3
-from models.base_model import BaseModel
-import datetime
+import cmd
 
-date = datetime.datetime.now()
-my_model = BaseModel()
-# my_model.name = "My First Model"
-# my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key,
-          type(my_model_json[key]), my_model_json[key]))
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Defines the HBNBCommand class
+    """
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """
+        EOF command to exit the program
+        """
+        return True
+
+    def emptyline(self):
+        """
+        Do nothing when an empty line is entered
+        """
+        return False
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
