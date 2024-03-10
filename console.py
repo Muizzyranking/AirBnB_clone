349a69ba8c8d967bc053b0a1506c11643cd7ab83
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-03-10 08:12:09 +0100

    console

--- console.py
+++ console.py
@@ -1,8 +1,17 @@
-# import cmd
 #!/usr/bin/env python3
 from models.base_model import BaseModel
+import datetime
 
-my_modulde = BaseModel()
-my_modulde.name = "Muiz"
-my_modulde.my_number = 89
-print(my_modulde)
+date = datetime.datetime.now()
+my_model = BaseModel()
+# my_model.name = "My First Model"
+# my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key,
+          type(my_model_json[key]), my_model_json[key]))
